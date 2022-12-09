fbfdac64dc13b4230589716d45d76a4e0990cfdc
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-12-09 21:31:46 +0700

    imperative programming

--- components/card.jsx
+++ components/card.jsx
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const MeetupCard = props => {
   const { imageUrl, title, description, address, id } = props
+  const router = useRouter()
+
+  const toMeetupDetail = () => {
+    router.push(`/${id}`)
+  }
 
   return (
     <div>
       <img src={imageUrl} width={100} height={200} alt="alt img" />
       <h4>{title}</h4>
       <address>{address}</address>
+
+      <button onClick={toMeetupDetail}>Detail</button>
     </div>
   )
 }
