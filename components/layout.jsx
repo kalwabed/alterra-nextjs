ccd22b83360fcb207a00604ff2bbbf95622ad36d
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-12-11 08:53:02 +0700

    styling layout

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,18 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
 
+import styles from './layout.module.css'
+
 const Layout = ({ children }) => {
   return (
     <>
-      <header>
-        <img src="https://picsum.photos/50" width={50} alt="logo" className="logo" />
+      <header className={styles.header}>
+        <img src="https://picsum.photos/50" width={50} height={50} alt="logo" className={styles.logo} />
         <nav>
-          <ul>
-            <li>
-              <Link href="/">All Meetups</Link>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Link href="/" className={styles.a}>
+                All Meetups
+              </Link>
             </li>
-            <li>
-              <Link href="/new">Add New Meetup</Link>
+            <li className={styles.li}>
+              <Link href="/new" className={styles.a}>
+                Add New Meetup
+              </Link>
             </li>
           </ul>
         </nav>
