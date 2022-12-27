2992318e9853f3c226cd2e0931bdbf04b85278e7
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-12-28 06:49:17 +0700

    adding non sense comment

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,14 @@ import MeetupCard from '../components/card'
 import styles from '../styles/home.module.css'
 
 export const getServerSideProps = async () => {
+  // melakukan request ke API route jika dari SSR harus menyertakan alamat lengkap.
+  // ketika di production, kita bisa mengganti localhost:3000 dengan domain
   const response = await fetch('http://localhost:3000/api/meetups')
   const data = await response.json()
 
   return {
     props: {
+      // jika data.meetups tidak ada, maka akan diisi dengan array kosong
       meetups: data.meetups ?? []
     }
   }
--- pages/new.jsx
+++ pages/new.jsx
@@ -3,9 +3,10 @@ import MeetupForm from '../components/meetup-form'
 
 const NewMeetupPage = () => {
   const onSubmit = async meetupData => {
+    // mengirim request ke API route yang dihandle di file api/meetups.js
     const response = await fetch('/api/meetups', {
       method: 'POST',
-      body: JSON.stringify(meetupData),
+      body: JSON.stringify(meetupData), // body harus dalam bentuk JSON
       headers: {
         'Content-Type': 'application/json'
       }
